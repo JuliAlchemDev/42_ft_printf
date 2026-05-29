4ec87e67f96660b7f916cb071f06c0535577df4b
Julia Alkhimova <devalchem@gmail.com>
2026-05-29 12:16:28 +0200

    chore: explore printf specifiers behaviour in main

--- ft_printf.c
+++ ft_printf.c
@@ -2,6 +2,7 @@
 #include <string.h>
 
 // Discovering how printf works on backstage  
+/*
 int main() {
     // 
     printf("return: %d\n", printf("Hello %s\n", "World!"));
@@ -9,5 +10,47 @@ int main() {
     char *str = "Hello World!";
     printf("Str len: %d", strlen(str));
     // returns 12: strlen doesn't count null terminator \0;
+    return 0;
+}
+*/
+
+// Learn more about printf specifiers to implement for this project
+int main() {
+ 
+    char ch = 'A';
+    
+    char str[10] = "Hello";
+    char *str_lit = "Hello";
+  
+    printf("Char: %c\n", 'A');
+    printf("String: %s\n", "Hello");
+    // STACK
+    printf("Pointer CH: %p\n", &ch); // Ex: 0x7ffcdd256cc7 (changes each run)
+    printf("Pointer STR: %p\n", &str); // Ex: 0x7ffcdd256cbd (changes each run)
+    // .rodata
+    printf("Pointer STR_lit: %p\n", str_lit); // 0x402004 (stable address)
+
+    int decimal = 9;
+    int integer = -42; 
+    unsigned int un_int = 42;
+    // unsigned int un_int = -42; // testing negative value → 2³² + (-42) = 4294967254
+
+    // same behaviour for printf
+    printf("Decimal: %d\n", decimal);
+    printf("Integer: %i\n", integer);
+    // if (positive ) returns num;
+    // if (negative) reinterprets bits as unsigned 2³² + num;
+    printf("Unsigned decimal / integer: %u\n", un_int);
+   
+
+    // TODO: discover what hex int is, usage and behaviour 
+    // printf("Hexadecimal integer (lower): %x\n");
+    // printf("Hexadecimal integer (upper): %X\n");
+
+    printf("Percent symbol: %%\n"); 
+    // %% accepts no arguments, %  alone is undefined behaviour
+    // printf("Variation 1: %\n"); -> invalid conversion specifier '\x0a'
+    // printf("Variation 2: %%\n", '%'); -> data argument not used by format string
+
     return 0;
 }
