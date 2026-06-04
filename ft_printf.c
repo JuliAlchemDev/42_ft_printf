b27029cdd7e534ceb293071bcb9bbd08eb866fdf
Julia Alkhimova <devalchem@gmail.com>
2026-06-04 19:41:30 +0200

    feat: implement custom ft_printf function and compare its behaviour with the original in main

--- ft_printf.c
+++ ft_printf.c
@@ -16,3 +16,28 @@ static int handle_specifier(char specifier, va_list args)
     // if(specifier == 'p')
     return (0);
 }
+
+int ft_printf(char const *str, ...)
+{
+    va_list args;
+    va_start(args, str);
+    size_t i;
+    size_t len;
+    
+    i = 0;
+    len = 0;
+    while(str[i])
+    {
+        if(str[i] == '%')
+        {
+           len += handle_specifier(str[i + 1], args);
+           i += 2;
+        }
+        else
+        {
+            len += write(1, &str[i], 1);
+            i++; 
+        }
+    }
+    return (len);
+}
