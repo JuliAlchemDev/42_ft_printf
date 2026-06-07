edac4f9bba9387a41a67e2b1194e159b38ec978f
Julia Alkhimova <devalchem@gmail.com>
2026-06-07 13:26:01 +0200

    feat: implement ft_putnbr_u to handle an unsigned int and return its length

---
+++ ft_putnbr_u.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+
+int ft_putnbr_u(unsigned int nbr)
+{
+    int len;
+    char num;
+
+    len = 0;
+    if(nbr > 9)
+        len += ft_putnbr_u(nbr / 10);
+    num = (nbr % 10) + '0';
+    write(1, &num, 1);
+    len++;
+    return (len);
+}
