ee49d64c1b478eaa850e9d942ee3b3e5ce0b1990
Julia Alkhimova <devalchem@gmail.com>
2026-06-07 13:58:46 +0200

    feat: implement static handle_specifier for %d, %i, %u, %x and %X specifiers

--- ft_printf.c
+++ ft_printf.c
@@ -9,10 +9,13 @@ static int handle_specifier(char specifier, va_list args)
         return (ft_putchar('%'));
     if(specifier == 's')
         return (ft_putstr(va_arg(args, char *)));
+    if(specifier == 'd' || specifier == 'i')
+        return (ft_putnbr(va_arg(args, int)));
+    if(specifier == 'u')
+        return (ft_putnbr_u(va_arg(args, unsigned int)));
+    if(specifier == 'x' || specifier == 'X')
+        return (ft_putnbr_hex(va_arg(args, unsigned int), specifier));
     // TODO
-    // if(specifier == 'd' || specifier == 'i')
-    // if(specifier == 'u')
-    // if(specifier == 'x' || specifier == 'X')
     // if(specifier == 'p')
     return (0);
 }
