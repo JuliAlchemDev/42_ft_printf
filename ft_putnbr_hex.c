1cd76aa786041155414243999b02dd315a6e6ad2
Julia Alkhimova <devalchem@gmail.com>
2026-06-07 13:53:54 +0200

    feat: implement ft_putnbr_hex to handle hex format and return its length

---
+++ ft_putnbr_hex.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+
+int ft_putnbr_hex(unsigned int nbr, char format)
+{
+    char *base;
+    int i; /* index */ 
+    int len;
+
+    len = 0;
+    if (format == 'x')
+        base = "0123456789abcdef";
+    else if (format == 'X')
+        base = "0123456789ABCDEF";
+    if(nbr > 15)
+        len += ft_putnbr_hex(nbr / 16, format);
+    i = nbr % 16;
+    write(1, &base[i], 1);
+    len++;
+    return (len);
+}
