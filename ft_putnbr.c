3619181f1d64ce4c758565cf712a8de36be38458
Julia Alkhimova <devalchem@gmail.com>
2026-06-07 12:54:06 +0200

    feat: implement ft_putnbr to write an int and return its length

---
+++ ft_putnbr.c
@@ -0,0 +1,26 @@
+#include <unistd.h>
+
+int ft_putnbr(int nbr)
+{
+    int len;
+    char num;
+
+    len = 0;
+    if (nbr == -2147483648)
+    {
+        write(1, "-2147483648", 11);
+        return (11);
+    }
+    if (nbr < 0)
+    {
+        write(1, "-", 1);
+        len++;
+        nbr *= -1;
+    }
+    if(nbr > 9)
+        len += ft_putnbr(nbr / 10);
+    num = (nbr % 10) + '0';
+    write(1, &num, 1);
+    len++;
+    return (len);
+}
