6dd3c10806cad24fa92f50cd1a3b87ff341ea50b
Iuliia Alkhimova <iualkhim@cbr2s5.42barcelona.com>
2026-06-09 16:31:28 +0200

    fix: handle NULL string in ft_putstr

--- ft_putstr.c
+++ ft_putstr.c
@@ -6,7 +6,7 @@
 /*   By: iualkhim <iualkhim@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/08 18:57:35 by iualkhim          #+#    #+#             */
-/*   Updated: 2026/06/08 18:58:41 by iualkhim         ###   ########.fr       */
+/*   Updated: 2026/06/09 16:25:28 by iualkhim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ int	ft_putstr(char *str)
 {
 	int	i;
 
+	if (!str)
+		str = "(null)";
 	i = 0;
 	while (str[i])
 	{
